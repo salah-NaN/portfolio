1b59e055f0824eabdd649bf111afd024bb74426e
Salah Eddin <salah.extras@gmail.com>
2024-05-08 17:27:08 +0200

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-amber-500">asdfdsaf</div>
+      <Button>Press here</Button>
+    </>
+  );
+};
+
+export default App;
